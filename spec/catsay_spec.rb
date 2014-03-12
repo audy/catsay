016429d72cf3a8a119641d7df1059d8200baa68d
audy <harekrishna@gmail.com>
2014-03-12 09:23:40 -0400

    tempfile

--- spec/catsay_spec.rb
+++ spec/catsay_spec.rb
@@ -4,7 +4,6 @@ describe Catsay do
 
   let(:kitty) { Catsay::Cat.new(:template => "<%= @message %>") }
   let(:message) { 'Meow, world!' }
-#  let(:tempfile) { Tempfile.new('meow') }
 
   it 'can meow a message' do
     kitty.meow(message).strip.should == message
