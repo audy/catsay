5c4bc0e90e7b7ceae0bd579a54d611d8ec4bf52a
audy <harekrishna@gmail.com>
2014-04-02 22:34:09 -0400

    added another test case

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,4 +4,8 @@ describe 'command-line interface' do
   it 'displays the help' do
     `bin/catsay --help`.should_not be_nil
   end
+
+  it 'can display a cat' do
+    `bin/catsay --cat test test`.strip.should == 'A kitty meows, "test"'
+  end
 end
